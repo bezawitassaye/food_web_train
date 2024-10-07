d409a3c0b743b5d6d27974075201987bad9c1d1e
bezawitassaye <beza1221love@gmail.com>
2024-10-07 13:20:45 -0400

    finish ui of explore page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
 import Header from '../../components/Header/Header'
+import Explore from '../../components/Explore/Explore'
 const Home = () => {
+  const [category,setcategory]=useState("All")
   return (
     <div>
       <Header/>
+      <Explore category={category} setcategory={setcategory}/>
     </div>
   )
 }
