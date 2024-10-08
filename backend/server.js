f184c92cb715a4307ce1756c1b0bd6b6cfec2d51
bezawitassaye <beza1221love@gmail.com>
2024-10-08 03:13:53 -0400

    connect with mongodb and create model for food

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import   "dotenv/config"; // Add this line
+import connectdb from "./config/db.js";
+
+// Add this line
+
+const app = express();
+const port = 4000;
+connectdb();
+
+app.use(express.json());
+app.use(cors());
 
 
-const app = express()
-const port = 4000
 
-app.use(express.json())
-app.use(cors())
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
 
-app.listen(port,()=>{
-    console.log(`server started on port ${port}`);
-    
-})
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+
