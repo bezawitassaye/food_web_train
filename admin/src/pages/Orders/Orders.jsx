9080103117185bc3f7865a5945f2a403df8b3225
bezawitassaye <beza1221love@gmail.com>
2024-10-09 02:30:20 -0400

    connect admin order page with api

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,30 +1,64 @@
-import React from 'react'
-import "./Orders.css"
-import { useState } from 'react'
-import axios from "axios"
-import { toast } from 'react-toastify'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import "./Orders.css";
+import axios from "axios";
+import { toast } from 'react-toastify';
+import { assets } from '../../assets/assets';
+
 const Orders = () => {
-  const backendurl="http://localhost:4000"
-  const [orders,setorders]=useState([])
-  const fetchallorders= async()=>{
-    const response = await axios.post(backendurl+"/api/order/listoredrs")
-    console.log(response)
-    if(response.data.success){
-      setorders(response.data.data)
-    }else{
-      toast.error("Error")
+  const backendurl = "http://localhost:4000";
+  const [orders, setorders] = useState([]);
+
+  const fetchallorders = async () => {
+    try {
+      const response = await axios.post(backendurl + "/api/order/listoredrs");
+      console.log(response);
+      if (response.data.success) {
+        setorders(response.data.data);
+      } else {
+        toast.error("Error");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Error fetching orders");
     }
-  }
-  useEffect(()=>{
-    fetchallorders()
-  },[])
+  };
+
+  useEffect(() => {
+    fetchallorders();
+  }, []);
+
+  console.log(orders);
+
   return (
-    <div>
-      Orders
-      
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {
+          orders.map((order, index) => {
+            return ( // Added return here
+              <div key={index} className='order-item'>
+                <img src={assets.parcel_icon} alt="" />
+                <div>
+                  <p className='order-item-food'>
+                    {
+                      order.items.map((item, index) => {
+                        if (index === order.items.length - 1) {
+                          return item.name + " x " + item.quantity;
+                        } else {
+                          return item.name + " x " + item.quantity + ", ";
+                        }
+                      })
+                    }
+                  </p>
+                  
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default Orders
+export default Orders;
